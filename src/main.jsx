4c27088a10c28c38ac5b002ab28039395534af40
Mahavr <mahavr2122003@gmail.com>
2024-12-10 00:09:53 +0200

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App.jsx";
+import App from "./Components/App.jsx";
 import "./index.css";
 import "modern-normalize";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
