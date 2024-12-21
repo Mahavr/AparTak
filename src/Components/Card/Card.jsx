7fddd66864c5464882aad66eb1677fe3fe4133d4
Mahavr <mahavr2122003@gmail.com>
2024-12-21 21:26:27 +0200

    fix home-card list

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,11 +2,9 @@ import css from "./Card.module.css";
 
 export const Card = ({ icon, children }) => {
   return (
-    <li>
-      <div className={css.card}>
-        <img src={icon} className={css.icon} />
-        <p> {children}</p>
-      </div>
-    </li>
+    <div className={css.card}>
+      <img src={icon} className={css.icon} />
+      <p> {children}</p>
+    </div>
   );
 };
